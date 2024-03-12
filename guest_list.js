12e5034c8906731295b3556856fe7fedd968fe83
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-12 21:13:13 +0500

    Add files via upload

---
+++ guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Nawaz Sharif', 'Inmran Khan', 'Asif Ali Zardari'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our Party.\n\nThank you!');
+}
---
+++ guest_list.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_list :string[]= ['Nawaz Sharif', 'Inmran Khan', 'Asif Ali Zardari'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our Party.\n\nThank you!')
+}
